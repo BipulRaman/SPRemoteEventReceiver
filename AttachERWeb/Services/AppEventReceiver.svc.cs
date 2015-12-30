0b5733476b0dfa15ebe478cf3d6cd2592e51c602
bipulraman <bipulraman@gmail.com>
2015-12-30 20:36:16 +0530

    Code commited

---
+++ AttachERWeb/Services/AppEventReceiver.svc.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace AttachERWeb.Services
+{
+
+    
+    public class AppEventReceiver : IRemoteEventService
+    {
+        private const string ReceiverName = "ItemAddedEvent";
+        private const string ListName = "MyAnnouncements";
+
+        /// <summary>
+        /// Handles app events that occur after the app is installed or upgraded, or when app is being uninstalled.
+        /// </summary>
+        /// <param name="properties">Holds information about the app event.</param>
+        /// <returns>Holds information returned from the app event.</returns>
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            SPRemoteEventResult result = new SPRemoteEventResult();
+
+            //using (ClientContext clientContext = TokenHelper.CreateAppEventClientContext(properties, useAppWeb: false))
+            //{
+            //    if (clientContext != null)
+            //    {
+            //        clientContext.Load(clientContext.Web);
+            //        clientContext.ExecuteQuery();
+            //    }
+            //}
+
+
+            switch (properties.EventType)
+            {
+                case SPRemoteEventType.AppInstalled:
+                    HandleAppInstalled(properties);
+                    break;
+                case SPRemoteEventType.AppUninstalling:
+                    HandleAppUninstalling(properties);
+                    break;
+                case SPRemoteEventType.ItemAdded:
+                    HandleItemAdded(properties);
+                    break;
+                
+            }            
+            return result;
+        }
+
+        /// <summary>
+        /// This method is a required placeholder, but is not used by app events.
+        /// </summary>
+        /// <param name="properties">Unused.</param>
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void HandleAppInstalled(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                if (clientContext != null)
+                {
+                    List myList = clientContext.Web.Lists.GetByTitle(ListName);
+                    clientContext.Load(myList, p => p.EventReceivers);
+                    clientContext.ExecuteQuery();
+                    
+                    bool rerExists = false;
+
+                    foreach (var rer in myList.EventReceivers)
+                    {
+                        if (rer.ReceiverName == ReceiverName)
+                        {
+                            rerExists = true;
+                            System.Diagnostics.Trace.WriteLine("Found existing ItemAdded receiver at "  + rer.ReceiverUrl);
+                        }
+                    }
+
+                    if (!rerExists)
+                    {
+                        EventReceiverDefinitionCreationInformation receiver =  new EventReceiverDefinitionCreationInformation();
+                        receiver.EventType = EventReceiverType.ItemAdded;
+
+                        //Get WCF URL where this message was handled
+                        OperationContext op = OperationContext.Current;
+                        Message msg = op.RequestContext.RequestMessage;
+                        receiver.ReceiverUrl = msg.Headers.To.ToString();
+                        receiver.ReceiverName = ReceiverName;
+                        receiver.Synchronization = EventReceiverSynchronization.Synchronous;
+                        myList.EventReceivers.Add(receiver);
+
+                        clientContext.ExecuteQuery();
+
+                        System.Diagnostics.Trace.WriteLine("Added ItemAdded receiver at " + msg.Headers.To.ToString());
+                    }
+                }
+            }
+        }
+
+
+        private void HandleAppUninstalling(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                if (clientContext != null)
+                {
+                    List myList = clientContext.Web.Lists.GetByTitle(ListName);
+                    clientContext.Load(myList, p => p.EventReceivers);
+                    clientContext.ExecuteQuery();
+
+                    var rer = myList.EventReceivers.Where(e => e.ReceiverName == ReceiverName).FirstOrDefault();
+
+                    try
+                    {
+                        System.Diagnostics.Trace.WriteLine("Removing ItemAdded receiver at " + rer.ReceiverUrl);
+
+                        //This will fail when deploying via F5, but works
+                        //when deployed to production
+
+                        rer.DeleteObject();
+                        clientContext.ExecuteQuery();
+                    }
+
+                    catch (Exception oops)
+                    {
+                        System.Diagnostics.Trace.WriteLine(oops.Message);
+                    }
+                }
+            }
+        }
+
+
+
+        private void HandleItemAdded(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+
+            {
+                if (clientContext != null)
+                {
+                    try
+                    {
+                        List photos = clientContext.Web.Lists.GetById(properties.ItemEventProperties.ListId);
+                        ListItem item = photos.GetItemById(properties.ItemEventProperties.ListItemId);
+                        clientContext.Load(item);
+                        clientContext.ExecuteQuery();
+                        
+                        item["Title"] += "\nUpdated by RER " +  System.DateTime.Now.ToLongTimeString();
+                        item["Body"] += "\n ~ Updated by RER at " + System.DateTime.Now.ToLongTimeString() + " -|- Developed by Bipul!  ";
+                        item.Update();
+                        clientContext.ExecuteQuery();
+                    }
+
+                    catch (Exception oops)
+                    {
+                        System.Diagnostics.Trace.WriteLine(oops.Message);
+                    }
+                }
+            }
+        }        
+    }
+}
